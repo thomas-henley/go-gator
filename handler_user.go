dd3f88d54f07f7b558546d310bba5b4a454e676d
Thomas Henley <thomas.e.henley@gmail.com>
2025-08-29 13:07:42 -0400

    consolidate user function handlers

--- handler_user.go
+++ handler_user.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/thomas-henley/go-gator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -21,5 +25,65 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Printf("logged in as user: %s\n", name)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+    if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	currentTime := time.Now().UTC()
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: currentTime,
+		UpdatedAt: currentTime,
+		Name: name,
+		})
+	if err != nil {
+        return fmt.Errorf("error creating user: %w", err)
+	}
+
+	s.cfg.SetUser(name)
+
+	fmt.Printf("User \"%s\" has been created\n", user.Name)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.Args) > 0 {
+		return fmt.Errorf("usage: %s", cmd.Name)
+	}
+
+	err := s.db.Reset(context.Background())
+	if err != nil {
+		return fmt.Errorf("error resetting the user database: %w", err)
+	}
+
+	fmt.Printf("database has been reset\n")
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	if len(cmd.Args) != 0 {
+		return fmt.Errorf("usage: %s\n", cmd.Name)
+	}
+
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("error fetching users: %w\n", err)
+	}
+
+    for _, user := range(users) {
+		loginStatus := ""
+		if (user.Name == s.cfg.CurrentUserName) {
+			loginStatus = " (current)"
+		}
+		fmt.Printf("* %s%s\n", user.Name, loginStatus)
+		
+	}
+
 	return nil
 }
